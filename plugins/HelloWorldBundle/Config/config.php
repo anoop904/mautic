00b54db0e7d24148c4fd65c870c8f19db28c3e84
Anoop <ideapower.anoop@gmail.com>
2017-03-23 17:53:50 +0530

    hello world plugin

---
+++ plugins/HelloWorldBundle/Config/config.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    'name'          => 'Hello World',
+    'description'   => 'This is an example config file for a simple Hello World addon.',
+    'version'       => '0.1.0',
+);
